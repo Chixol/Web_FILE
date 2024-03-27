29b0c7646ea0a593da0d0e3b22fc7e7f017a6011
Chixol <phw9278@gmail.com>
2024-03-27 09:49:51 +0900

    2024-03-27 09:00

--- estate/src/sign-in/script.js
+++ estate/src/sign-in/script.js
@@ -14,23 +14,9 @@ function onSignInButtonClickHandler(event) {
 }
 
 function onSignInButtonClickHandler(event) {
-    window.location.href = 'https://google.com';
+    window.location.href = '../sign-up';
 }
 
 const signUpLinkElement = document.getElementById('sign-up-link');
 signUpLinkElement.addEventListener('click', onSingUpLinkClickHandler);
 
-// kakao https://developers.kakao.com/
-function onKakaoButtonClickHandler(event) {
-    window.location.href = 'https://developers.kakao.com/';
-}
-
-// naver https://developers.naver.com/
-function onNaverButtonClickHandler(event) {
-    window.location.href = 'https://developers.naver.com/';
-}
-
-const kakaoButtonElement = document.getElementById('kakao-button');
-kakaoButtonElement.addEventListener('click', onKakaoButtonClickHandler);
-const naverButtonElement = document.getElementById('naver-button');
-naverButtonElement.addEventListener('click', onNaverButtonClickHandler);
