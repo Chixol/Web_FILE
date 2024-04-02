9a75ff283962bcc19573bda822bf88556b6b19f6
Chixol <phw9278@gmail.com>
2024-04-02 11:48:53 +0900

    2024-04-02 11:00

--- ts/5.type_alias/type_alias.ts
+++ ts/5.type_alias/type_alias.ts
@@ -1,9 +1,30 @@
 /*
     타입 앨리어스 :
-    - 
-    - 
+    - 커스텀 타입을 지정하는 방법으로 인터페이스와 유사하게 사용됨
+    - type 키워드를 사용
 */
+// let gender: '남' | '여'; // 아래같이 사용해야 함
 
+type Gender = '남' | '여';
+let gender: Gender = '남'; 
+
+type CustomText = string;
+
+console.log('==================================================');
+//아래와 같이 선언을 해도 쓸 수 있음
+type King = {
+    name: string,
+    tombName: string,
+    birth: string,
+    death: string
+};
+
+const taejo: King = {
+    name: '이성계',
+    tombName: '태조',
+    birth: '1335-11-04',
+    death: '1408-06-27'
+}
 
 
 console.log('==================================================');
